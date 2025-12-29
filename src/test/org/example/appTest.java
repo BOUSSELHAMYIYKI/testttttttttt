491c99bc0784b26ec37f13025671206f095063f1
Bousselham el-abd <bousselham.elabd2000@gmail.com>
2025-12-29 17:39:31 +0100

    first commit

--- src/test/org/example/appTest.java
+++ src/test/org/example/appTest.java
@@ -9,19 +9,4 @@ public class appTest {
     public void testAdd() {
         assertEquals(15, app.add(10, 5));
     }
-
-    @Test
-    public void testSubtract() {
-        assertEquals(5, app.subtract(10, 5));
-    }
-
-    @Test
-    public void testMultiply() {
-        assertEquals(50, app.multiply(10, 5));
-    }
-
-    @Test
-    public void testDivide() {
-        assertEquals(2.0, app.divide(10, 5));
-    }
 }
