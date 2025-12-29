30e3bea0029ea7b0c0bb2caf28f30334a388d8b8
Bousselham el-abd <bousselham.elabd2000@gmail.com>
2025-12-29 23:01:44 +0100

    first commit

--- src/test/org/example/appTest.java
+++ src/test/org/example/appTest.java
@@ -3,10 +3,25 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class appTest {
+class appTest {
 
     @Test
-    public void testAdd() {
+    void testAdd() {
         assertEquals(15, app.add(10, 5));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(5, app.subtract(10, 5));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(50, app.multiply(10, 5));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2.0, app.divide(10, 5));
     }
 }
